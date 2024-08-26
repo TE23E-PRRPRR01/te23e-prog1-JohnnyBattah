0c81cbfb4aaa07c47f3f5cd211ca57f0a028fcb8
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2024-08-26 14:31:51 +0200

    Tränat på xw, cr och svenska tecken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej världen!");
+Console.WriteLine("Nu mitt första C# program");;
+
+Console.Write("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej" + namn);
+
+Console.Write("vad gör du på fritiden?");
+string fritid = Console.ReadLine();
+Console.WriteLine("VAA!!!!???? Jag älkar också att " + fritid + "!!!!!");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.Write("Såg du på OS i sommar? Vilken sport gillar du mest?");
