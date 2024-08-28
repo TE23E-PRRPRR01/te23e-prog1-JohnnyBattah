be37cb54e8fce07b92e2232a3f19c301e0ff62d5
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2024-08-28 14:42:10 +0200

    Lucktext, cw och cr. if och else

---
+++ Kapitel-1/Lucktext/Program.cs
@@ -0,0 +1,28 @@
+﻿// program för lucktext
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("En rolig lucktext!");
+
+Console.Write("Vad heter du?: ");
+string namn = Console.ReadLine();
+if (namn == "")
+{
+    Console.WriteLine("Error! Du glömde skriva vad du heter");
+}
+else
+{
+    Console.Write("Vad är din favorit maträtt?: ");
+string mat = Console.ReadLine();
+
+Console.Write("Vad är din favorit semester destination?: ");
+string semester = Console.ReadLine();
+
+Console.Write("Vem är din favorit idrottare?: ");
+string idrottare = Console.ReadLine();
+
+Console.WriteLine($"Hej! Jag heter {namn} och min dröm är att äta {mat} i {semester} med {idrottare}");
+}
+
