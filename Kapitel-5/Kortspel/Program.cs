273d4ae20300bd6ea059007e5fc40b698e0b0dd6
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2024-09-30 15:08:10 +0200

    Listor, emoji, random

---
+++ Kapitel-5/Kortspel/Program.cs
@@ -0,0 +1,35 @@
+﻿// Hur man skapar och använder listor
+Console.Clear();
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Slumpa kort ur ett kortlek!");
+
+// Ett annat exempel. lista på 3 årtal
+// List<int> årtal = [1957, 1969, 1989];
+
+// Skapa en lista kort
+// List<string> kortlek = ["Ess", "Tvåa", "Trea", "Fyra", "Femma", "Sexa", "Sjua", "Åtta", "Nia", "Tia", "Kneckt", "Dam", "Kung"];
+// List<string> kortlek = ["🂡", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮	"];
+List<string> kortlek = ["😃", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮	"];
+
+
+int antal = 5;
+while (antal > 0)
+{
+    // Slumpa index 0-12
+    int index = Random.Shared.Next(0, kortlek.Count);
+    // Console.WriteLine($"Det slumpade indexet = {index}");
+
+
+    // Plocka ut det sjunde kortet, index = random
+    string kort = kortlek[index];
+
+
+     // Ta bort staden från städerma
+    kortlek.RemoveAt(index);
+
+    // Skriv ut det slumpade kortet
+    Console.WriteLine($"Det slumpade kortet är {kort}");
+
+    // Räkna ned
+    antal--;
+}
---
+++ Kapitel-5/SlumpaStader/Program.cs
@@ -0,0 +1,29 @@
+﻿// Slumpa 2 av 10 städer
+Console.Clear();
+Console.WriteLine("Slumpa 2 städer!");
+
+// Skapa en lista städer
+List<string> städer = ["Stockholm", "Hawaii", "Baghdad", "Gaza", "Sarajevo", "Berlin", "Dortmund", "Amsterdam", "Alanya", "Antalya"];
+
+
+int antal = 2;
+while (antal > 0)
+{
+    // Slumpa index 0-9
+    int index = Random.Shared.Next(0, städer.Count);
+    // Console.WriteLine($"Det slumpade indexet = {index}");
+
+    // Plocka ut det sjunde kortet, index = random
+    string stad = städer[index];
+
+
+    // Skriv ut det slumpade staden
+    Console.WriteLine($"Det slumpade staden är {stad}");
+
+    
+    // Ta bort staden från städerma
+    städer.RemoveAt(index);
+
+    // Räkna ned
+    antal--;
+}
