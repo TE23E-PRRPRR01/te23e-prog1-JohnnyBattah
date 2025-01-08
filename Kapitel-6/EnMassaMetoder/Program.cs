9f418434ba7707055fa2b6da27bb8ba65f3ae9b3
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2025-01-08 15:05:56 +0100

    Tränat på metoder

---
+++ Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,25 @@
+﻿/* *********************************************************
+* *********************** M A I N ***********************
+************************************************************ */
+
+Console.Clear();
+Console.WriteLine("Hej Metoder!");
+
+SägHejNamn("Valentin");
+SägHejNamn("Johnny");
+SägHejNamn("Benjamin");
+SägHejNamn("");
+
+
+/* *********************************************************
+* ******************** M E T O D E R ***********************
+************************************************************ */
+
+/// <summary>
+/// Skrivut hälsning
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Hej på dig {namn}!");
+}
---
+++ Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,53 @@
+﻿/* *********************************************************
+* ******************   M A I N   ***************************
+************************************************************ */
+
+Console.Clear();
+Console.WriteLine("Hej metoder!");
+
+// Anropa/Använd metoden
+SägHej();
+SägHej();
+SägHej();
+SägHej();
+SägHej();
+
+// Anropa med metoden meny
+VisaMeny();
+
+// Anropa metoden SägGrattis
+SägGrattis();
+
+
+/* *********************************************************
+* ******************** M E T O D E R ***********************
+************************************************************ */
+
+/// <summary>
+/// Metod som skriver ut en hälsning 
+/// </summary>
+static void SägHej()
+{
+    Console.WriteLine("Hej på dig!");
+}
+
+/// <summary>
+/// Metod för att visa en meny
+/// </summary>
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Skriv ut alla uppgifter
+    4. Avsluta
+    """);
+}
+
+/// <summary>
+/// Metod som gratulerar på födelsedagen
+/// </summary>
+static void SägGrattis()
+{
+    Console.WriteLine("Grattis på födelsedagen!");
+}
